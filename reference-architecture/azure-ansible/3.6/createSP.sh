9965376c93c5e7ee9135e98c5ecd34f0b18a59dc
Jason Horn <jason.horn@gmail.com>
2018-03-06 16:45:58 -0500

    added git post-checkout hook and azure scripts

--- reference-architecture/azure-ansible/3.6/createSP.sh
+++ reference-architecture/azure-ansible/3.6/createSP.sh
@@ -3,6 +3,7 @@
 # $2 is azure password
 # $3 is service principal name
 # $4 is service principal password
+# $5 is Subscription , can be string to search for or complete subscription name
 
 # handled with playbook prepare
 #yum -y install wget
@@ -12,46 +13,54 @@
 #pip install --upgrade pip && pip install azure-cli
 
 function setup {
-tempFile="${HOME}/.az_account"
-echo ${tempFile}
+  tempFile="${HOME}/.az_account"
 
-echo "az login -u ${az_name} -p \'${az_pass}\'"
-az login -u ${az_name} -p \'${az_pass}\'
-az account show > ${tempFile}
+  echo "az login -u ${az_name}"
+  # check if logged in already
+  az account list -o table > ${tempFile}  2>/dev/null && grep -Eq "${az_sub}" ${tempFile}
+  if [[ $? != 0 ]]; then
+    echo "need to login"
+    az login
+  fi
 
-subscription_id=$(awk -F'"' '/\"id\"\:/ {print $4}' ${tempFile})
-client_id=$(az ad app show --id "http://${sp_name}"|awk -F'"' '/appId/  {print $4}')
-tenant_id=$(az account show|awk -F'"' '/\"tenantId"\:/ {print $4}' ${tempFile} )
+  subscription_id=$(awk "/${az_sub}/ {print \$3}"  ${tempFile})
+  subscription_name=$(awk "/${az_sub}/ {print \$1}" ${tempFile})
+  az account set --subscription ${subscription_id}
 
-echo "Creating SP ${sp_name}"
-az ad sp create-for-rbac -n ${sp_name} --role contributor --password ${sp_pass} \
- --scope /subscriptions/${subscription_id}
-
-echo "Creating Credentials ~/.azure/credentials"
-cat >  ~/.azure/credentials <<EOF
-[default]
+  echo "Creating SP ${sp_name}"
+  az ad sp create-for-rbac -n ${sp_name} --role contributor --password ${sp_pass} \
+   --scope /subscriptions/${subscription_id}
+  
+  client_id=$(az ad app show --id "http://${sp_name}"|awk -F'"' '/appId/  {print $4}')
+  tenant_id=$(az account show|awk -F'"' '/\"tenantId"\:/ {print $4}' )
+  
+  echo "Creating Credentials ~/.azure/credentials"
+  cat >  ~/.azure/credentials <<EOF
+[${subscription_name}]
 subscription_id=${subscription_id}
 tenant=${tenant_id}
 client_id=${client_id}
 secret=${sp_pass}
 EOF
 
-echo "Credentials:"
-echo -e "  Azure Login:     ${az_name}"
-echo -e "  Subscription ID: ${subscription_id}"
-echo -e "  Tenant ID:       ${tenant_id}"
-echo -e "  SP Name:         ${sp_name}"
-echo -e "  SP ID:           ${client_id}"
+  echo "Credentials:"
+  echo -e "  Azure Login:      ${az_name}"
+  echo -e "  Subscription ID:  ${subscription_id}"
+  echo -e "  Subscription:     ${subscription_name}"
+  echo -e "  Tenant ID:        ${tenant_id}"
+  echo -e "  SP Name:          ${sp_name}"
+  echo -e "  SP ID:            ${client_id}"
 
 }
 
 if [[ $# < 4 ]]; then
   echo "Error: Requires 4 args"
-  echo -e "Usage: $0 [azure login] [azure password] [service principal name to create] [service principal password]"
+  echo -e "Usage: $0 [azure login] [azure password] [service principal name to create] [service principal password] [subscription  PROD|DEV ] "
 else
   az_name=${1}
   az_pass=${2}
   sp_name=${3}
   sp_pass=${4}
+  az_sub=${5}
   setup
 fi
--- reference-architecture/azure-ansible/3.6/manageSaFiles.sh
+++ reference-architecture/azure-ansible/3.6/manageSaFiles.sh
@@ -1,5 +1,7 @@
 #!/bin/env bash
 
+# Azure Subscription to use
+AZ_SUB="SPEC.DEV"
 # Resource group for blob container
 RESOURCE_GROUP="openshift-rg"
 # Storage Account for blob container 
@@ -8,28 +10,59 @@ STORAGE_CONTAINER="ocp-dev"
 # Template that contains variables pointing to this container
 ARM_TEMPLATE="azuredeploy.json.sa"
 FILES="${ARM_TEMPLATE}  azuredeploy.parameters.json bastion.json bastion.sh infranode.json master.json master.sh node.sh node.json"
-AZ_STORAGE_ACCESS_KEY=$(azure storage account keys list -g ${RESOURCE_GROUP} ${STORAGE_ACCOUNT} |awk '/key1/ {print $3}')
+
+
+function usage {
+  echo "Error: Requires 3 args"
+  echo -e "Usage: $0 [azure login] [azure password] [upload|delete]"
+  exit 1
+}
+
+function setup {
+  tempFile="${HOME}/.az_account"
+  
+  echo "az login -u ${az_name}"
+  # check if logged in already
+  az account list -o table > ${tempFile}  2>/dev/null && grep -Eq "${AZ_SUB}" ${tempFile}
+  if [[ $? != 0 ]]; then 
+    echo "need to login"
+    az login 
+  fi
+
+  subscription_id=$(awk "/${AZ_SUB}/ {print \$3}"  ${tempFile})
+  subscription_name=$(awk "/${AZ_SUB}/ {print \$1}" ${tempFile})
+  az account set --subscription ${subscription_id}
+ }
+
+AZ_STORAGE_ACCESS_KEY=$(az storage account keys list -g ${RESOURCE_GROUP} -n ${STORAGE_ACCOUNT} -o table | awk '/key1/ {print $3}')
 
 export AZURE_STORAGE_ACCESS_KEY=${AZ_STORAGE_ACCESS_KEY}
 export AZURE_STORAGE_ACCOUNT=${STORAGE_ACCOUNT}
 
 function upload() {
-# upload files to blob storage
-for file in ${FILES}; do
-  azure storage blob upload -q ${file} ${STORAGE_CONTAINER}
-done
+  # upload files to blob storage
+  for file in ${FILES}; do
+    az storage blob upload  -f ${file} -c ${STORAGE_CONTAINER}   -n ${file}
+  done
 }
 
 function delete() {
-for file in ${FILES}; do azure storage blob delete  --delete-snapshots='include' -q  ${STORAGE_CONTAINER} ${file}
-done
+  for file in ${FILES}; do 
+    az storage blob delete  --delete-snapshots='include'  -n ${file} -c ${STORAGE_CONTAINER} 
+  done
 }
 
-if  [[ $1 =~ delete|upload ]]; then
-  CMD=$1
+if [[ $# < 2 ]]; then
+  usage
 else
-  echo "Usage: $0 upload|delete"
-  exit 1
+  az_name=${1}
+  setup
+fi
+
+if  [[ $2 =~ delete|upload ]]; then
+  CMD=$2
+else
+  usage
 fi
 
 echo "${CMD}ing"
