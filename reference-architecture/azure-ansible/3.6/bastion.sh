1f284a8f262dcd1e7d1a76832f587e356df894e6
root <root@embryonic.li0yjhb5ppeu5fqbzxqhuksbvf.dx.internal.cloudapp.net>
2018-03-16 11:57:35 +0000

    disabled sso playbook on installation

--- reference-architecture/azure-ansible/3.6/bastion.sh
+++ reference-architecture/azure-ansible/3.6/bastion.sh
@@ -1627,7 +1627,7 @@ oc adm policy add-cluster-role-to-user cluster-admin ${AUSERNAME}
 # Workaround for BZ1469358
 ansible master1 -b -m fetch -a "src=/etc/origin/master/ca.serial.txt dest=/tmp/ca.serial.txt flat=true"
 ansible masters -b -m copy -a "src=/tmp/ca.serial.txt dest=/etc/origin/master/ca.serial.txt mode=644 owner=root"
-ansible-playbook /home/${AUSERNAME}/setup-sso.yml &> /home/${AUSERNAME}/setup-sso.out
+#ansible-playbook /home/${AUSERNAME}/setup-sso.yml &> /home/${AUSERNAME}/setup-sso.out
 cat /home/${AUSERNAME}/openshift-install.out | tr -cd [:print:] |  mail -s "${RESOURCEGROUP} Install Complete" ${RHNUSERNAME} || true
 touch /root/.openshiftcomplete
 touch /home/${AUSERNAME}/.openshiftcomplete
